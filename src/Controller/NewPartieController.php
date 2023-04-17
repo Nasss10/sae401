9a2b904e20a433414a0e016506d4383f98e92bde
Nassim Ouali <100594579+Nasss10@users.noreply.github.com>
2023-04-17 16:15:13 +0200

    modif

--- src/Controller/NewPartieController.php
+++ src/Controller/NewPartieController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\MotPartie;
 use App\Entity\Partie;
+use App\Form\PartieType;
 use App\Repository\MotPartieRepository;
 use App\Repository\MotRepository;
 use App\Repository\PartieRepository;
@@ -17,86 +18,87 @@ class NewPartieController extends AbstractController
 {
     #[Route('/new/partie', name: 'app_new_partie')]
     public function index(
-        Request $request,
+        Request             $request,
         MotPartieRepository $motPartieRepository,
-        PartieRepository $partieRepository,
-        MotRepository $motRepository,
-        UserRepository $userRepository
+        PartieRepository    $partieRepository,
+        MotRepository       $motRepository,
+        UserRepository      $userRepository
     ): Response
     {
-        $joueur1 = $joueur2 = $userRepository->find(1);
         $partie = new Partie();
-        $partie->setJoueur1($joueur1); //mettre $this->getUser()
+        $form = $this->createForm(PartieType::class, $partie);
 
-        //récupération du J2 depuis le formulaire
-        $joueur2 = $userRepository->find(2);
-        $partie->setJoueur2($joueur2);
-        $partie->setTourJoueur($joueur1);//mettre $this->getUser()
-        $partie->setEtatPartie('en cours');
-        $partie->setVictoire(false);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $partie = $form->getData();
+            $partie->setJoueur1($this->getUser());
+            $partie->setTourJoueur($this->getUser());
+            $partie->setEtatPartie('en cours');
+            $partie->setVictoire(false);
 
-        $partieRepository->save($partie,true);
+            $partieRepository->save($partie, true);
 
-        //récupération de 25 mots aléatoires
-        $mots = $motRepository->findAll();
-        $tMots = [];
-        foreach ($mots as $mot) {
-            $tMots[$mot->getId()] = $mot;
+            //récupération de 25 mots aléatoires
+            $mots = $motRepository->findAll();
+            $tMots = [];
+            foreach ($mots as $mot) {
+                $tMots[$mot->getId()] = $mot;
+            }
+
+            $tCartes = [];
+            $tCartes[0][1] = 'Noir';
+            $tCartes[0][2] = 'Vert';
+
+            $tCartes[1][1] = 'Noir';
+            $tCartes[1][2] = 'Noir';
+
+            $tCartes[2][1] = 'Noir';
+            $tCartes[2][2] = 'Neutre';
+
+            $tCartes[3][1] = 'Vert';
+            $tCartes[3][2] = 'Noir';
+
+            $tCartes[4][1] = 'Neutre';
+            $tCartes[4][2] = 'Noir';
+
+            $tCartes[5][1] = 'Vert';
+            $tCartes[5][2] = 'Vert';
+
+            $tCartes[6][1] = 'Vert';
+            $tCartes[6][2] = 'Vert';
+
+            $tCartes[7][1] = 'Vert';
+            $tCartes[7][2] = 'Vert';
+
+            for ($i = 8; $i < 15; $i++) {
+                $tCartes[$i][1] = 'Neutre';
+                $tCartes[$i][2] = 'Neutre';
+            }
+
+            for ($i = 15; $i < 20; $i++) {
+                $tCartes[$i][1] = 'Vert';
+                $tCartes[$i][2] = 'Neutre';
+            }
+
+            for ($i = 20; $i < 25; $i++) {
+                $tCartes[$i][1] = 'Neutre';
+                $tCartes[$i][2] = 'Vert';
+            }
+            shuffle($tCartes);
+            shuffle($tMots); //mélange du tableau
+
+            for ($i = 0; $i < 25; $i++) {
+                $mp = new MotPartie();
+                $mp->setPartie($partie);
+                $mp->setMot(array_pop($tMots));
+                $mp->setCouleurJ1($tCartes[$i][1]);
+                $mp->setCouleurJ2($tCartes[$i][2]);
+                $mp->setEmplacement($i);
+                $mp->setTrouve(false);
+                $motPartieRepository->save($mp, true);
+            }
+            return new Response(); // ajout de cette ligne pour renvoyer une réponse vide
+
         }
-
-        $tCartes = [];
-        $tCartes[0][1] = 'Noir';
-        $tCartes[0][2] = 'Vert';
-
-        $tCartes[1][1] = 'Noir';
-        $tCartes[1][2] = 'Noir';
-
-        $tCartes[2][1] = 'Noir';
-        $tCartes[2][2] = 'Neutre';
-
-        $tCartes[3][1] = 'Vert';
-        $tCartes[3][2] = 'Noir';
-
-        $tCartes[4][1] = 'Neutre';
-        $tCartes[4][2] = 'Noir';
-
-        $tCartes[5][1] = 'Vert';
-        $tCartes[5][2] = 'Vert';
-
-        $tCartes[6][1] = 'Vert';
-        $tCartes[6][2] = 'Vert';
-
-        $tCartes[7][1] = 'Vert';
-        $tCartes[7][2] = 'Vert';
-
-        for($i=8; $i<15; $i++) {
-            $tCartes[$i][1] = 'Neutre';
-            $tCartes[$i][2] = 'Neutre';
-        }
-
-        for($i=15; $i<20; $i++) {
-            $tCartes[$i][1] = 'Vert';
-            $tCartes[$i][2] = 'Neutre';
-        }
-
-        for($i=20; $i<25; $i++) {
-            $tCartes[$i][1] = 'Neutre';
-            $tCartes[$i][2] = 'Vert';
-        }
-        shuffle($tCartes);
-        shuffle($tMots); //mélange du tableau
-
-        for($i=0;$i<25;$i++) {
-            $mp = new MotPartie();
-            $mp->setPartie($partie);
-            $mp->setMot(array_pop($tMots));
-            $mp->setCouleurJ1($tCartes[$i][1]);
-            $mp->setCouleurJ2($tCartes[$i][2]);
-            $mp->setEmplacement($i);
-            $mp->setTrouve(false);
-            $motPartieRepository->save($mp,true);
-        }
-        return new Response(); // ajout de cette ligne pour renvoyer une réponse vide
-
     }
 }
