e84751305d1bf71c7471e5761d20757e4f248a80
Nassim Ouali <100594579+Nasss10@users.noreply.github.com>
2023-04-18 10:46:32 +0200

    modif

--- src/Controller/StatistiquesController.php
+++ src/Controller/StatistiquesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PartieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class StatistiquesController extends AbstractController
 {
     #[Route('/statistiques', name: 'app_statistiques')]
-    public function index(): Response
+    public function index(PartieRepository $partieRepository): Response
     {
+        $parties = $partieRepository->findAll();
+
         return $this->render('statistiques/index.html.twig', [
             'controller_name' => 'StatistiquesController',
+            'parties' => $parties
         ]);
     }
 }
