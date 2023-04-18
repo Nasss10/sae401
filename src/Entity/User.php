c34d1722b23b595e2e5801b4bbe3a45feb9d6c9e
Nassim Ouali <100594579+Nasss10@users.noreply.github.com>
2023-04-18 09:45:14 +0200

    modif

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -39,7 +39,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $pseudo = null;
 
     #[ORM\Column(length: 255, nullable: true)]
-    private ?string $pdp = null;
+    private ?string $pdp = '643e4a36799c1.jpg';
 
 
 
